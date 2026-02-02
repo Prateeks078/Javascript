d4680cd4d9c6dcb19daca3aa341669b4f195f70a
Prateek <prateek.sharma@humanpoweredhealth.com>
2026-02-02 11:14:31 +0530

    Adding Object Part 2

---
+++ W1/6_Objects.js
@@ -0,0 +1,14 @@
+
+
+// Methods && Functions 
+// Function is basically a stand alone quantity but if that function is defined inside an object then 
+// it is referred to us as a method 
+
+
+// Suppose I have an object Person1 with some properties and I want to make another object Person2 with same properties 
+// So i need to write the same code again and again for each object and there will be for sure chances of bugs 
+
+// I can't use Deep copy because it is used to copy the values of one object to another object but here
+//  I want to copy the structure of one object to another object or want to copy the keys na bro 
+
+
