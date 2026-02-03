ca85062897861c91e77d647a9c18359b86af5a81
Prateek <prateek.sharma@humanpoweredhealth.com>
2026-02-03 11:32:36 +0530

    Updating the todo app

--- W1/8_Todo/Todo.js
+++ W1/8_Todo/Todo.js
@@ -1,8 +1,35 @@
 // For Testing Purpose Only 
 // window.alert("Hey, Be cautious");
 
-
 // Task: Create a Todo List Application
-const iptext=document.getElementById("iptext");
-const val=iptext.innerText;
-console.log(val);
+const iptext = document.getElementById("iptext");
+const addbutton = document.getElementById("ipbutton");
+const container = document.getElementById("container");
+const listcontainer = document.getElementById("todolist-container");
+
+
+const todoinput = document.getElementById("iptext");
+
+addbutton.addEventListener('click', () => {
+    const val = iptext.value;
+    console.log("Value Entered by the user is: ", val);
+    iptext.value = "";
+
+    // Until this point, we have the value entered by the user in the input box.
+    // Now, we will be trying to save this value somehere okay
+
+    const li = document.createElement("li");
+    const cmark = document.createElement("button")
+    cmark.innerText = "x"
+    // Abhi tk li tag bn chuka h tera okay
+
+    cmark.addEventListener('click', () => {
+        listcontainer.removeChild(li);
+    })
+
+    li.innerText = val;
+    li.appendChild(cmark);
+    listcontainer.appendChild(li);
+})
+
+
